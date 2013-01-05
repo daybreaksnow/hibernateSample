f2406456f0e692388099e20d329c4ed13b8edf8d
TOSHIYUKI IMAIZUMI <daybreaksnow@gmail.com>
2013-01-05 23:07:06 +0900

    カスケードのテストコード追加

--- src/inaction/association/Main.java
+++ src/inaction/association/Main.java
@@ -1,5 +1,7 @@
 package inaction.association;
 
+import java.util.Set;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -7,9 +9,45 @@ import techscore.sample.DaoSupport;
 
 public class Main {
 	public static void main(String[] args) {
-		saveBid();
-		saveItem();
-		relation();
+		// saveBid();
+		// saveItem();
+		// relation();
+
+		// relationBidToItem();
+		// relationInterOther();
+
+		// relationItemToBid();
+
+		// deleteItem(19);
+		deleteItemSet(13);
+		System.err.println("end");
+	}
+
+	// cascade-allでitemを消したらbidも消えるかのテスト
+	private static void deleteItem(long itemId) {
+		DaoSupport dao = new DaoSupport();
+		Session session = dao.getSession();
+		Transaction tx = session.beginTransaction();
+
+		Item item = loadItem(session, itemId);
+
+		session.delete(item);
+		tx.commit();
+		session.close();
+	}
+
+	// cascade-all-delete-orphenでbidが消えるかのテスト
+	// NOTE:Bid→Itemもcascade-allだとItemも消えてしまう
+	private static void deleteItemSet(long itemId) {
+		DaoSupport dao = new DaoSupport();
+		Session session = dao.getSession();
+		Transaction tx = session.beginTransaction();
+
+		Item item = loadItem(session, itemId);
+		Set bids = item.getBids();
+		bids.clear();
+		tx.commit();
+		session.close();
 	}
 
 	private static void relation() {
@@ -23,6 +61,72 @@ public class Main {
 		bid.setItem(item);
 
 		session.save(bid);
+		tx.commit();
+		session.close();
+	}
+
+	//
+	private static void relationBidToItem() {
+		DaoSupport dao = new DaoSupport();
+		Session session = dao.getSession();
+		Transaction tx = session.beginTransaction();
+
+		Bid bid = new Bid();
+		Item item = new Item();
+		bid.setItem(item);
+		item.getBids().add(bid);
+
+		// NOTE: bid→itemへのcascadeがなく、itemをsaveしないと以下の例外が発生する
+		// org.hibernate.TransientObjectException: object references an unsaved
+		// transient instance - save the transient instance before flushing:
+		// inaction.association.Item
+		session.save(bid);
+		session.save(item);
+		tx.commit();
+		session.close();
+	}
+
+	// itemのみ保存
+	private static void relationItemToBid() {
+		DaoSupport dao = new DaoSupport();
+		Session session = dao.getSession();
+		Transaction tx = session.beginTransaction();
+
+		Bid bid = new Bid();
+		Item item = new Item();
+		// NOTE：ここでrefを入れておかないと、Bidは保存されるが、item_idがセットされない
+		bid.setItem(item);
+		item.getBids().add(bid);
+
+		// NOTE: bid→itemへのcascadeがなく、itemをsaveしないと以下の例外が発生する
+		// org.hibernate.TransientObjectException: object references an unsaved
+		// transient instance - save the transient instance before flushing:
+		// inaction.association.Item
+		session.save(item);
+		tx.commit();
+		session.close();
+	}
+
+	// 関連に不整合をつけて保存
+	// Bid→item1,item2→Bid
+	private static void relationInterOther() {
+		DaoSupport dao = new DaoSupport();
+		Session session = dao.getSession();
+		Transaction tx = session.beginTransaction();
+
+		Bid bid = new Bid();
+		Item item1 = new Item();
+		item1.setName("item1");
+		bid.setItem(item1);
+
+		Item item2 = new Item();
+		item2.setName("item2");
+		item2.getBids().add(bid);
+
+		// NOTE:item→bidsへのinverseがtrueならばitem1が、falseならばitem2のitem_idがセットされる
+		session.save(bid);
+		session.save(item1);
+		session.save(item2);
 		tx.commit();
 		session.close();
 	}
