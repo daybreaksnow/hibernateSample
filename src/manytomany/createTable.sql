baad7e8e33f35a48d1c3aa71f155b01f1a1fd26a
TOSHIYUKI IMAIZUMI <daybreaksnow@gmail.com>
2013-01-20 19:52:02 +0900

    manytomany用サンプル。ジェネレート直後

---
+++ src/manytomany/createTable.sql
@@ -0,0 +1,47 @@
+create table CATEGORY_MANYTOMANY(
+ category_id bigint not null,
+ parent_category_id bigint,
+ name varchar(256) not null,
+ version bigint
+);
+
+--Category PK
+ALTER TABLE CATEGORY_MANYTOMANY ADD PRIMARY KEY(category_id);
+
+--parentCategory FK
+ALTER TABLE CATEGORY_MANYTOMANY ADD CONSTRAINT CATEGORY_MANYTOMANY_PARENT_FK
+ FOREIGN KEY (parent_category_id)
+ REFERENCES CATEGORY_MANYTOMANY(category_id);
+
+
+--
+create table ITEM_MANYTOMANY(
+ item_id bigint not null,
+ name varchar(256),
+ version bigint
+);
+
+--Category PK
+ALTER TABLE ITEM_MANYTOMANY ADD PRIMARY KEY(item_id);
+
+--中間マッピング
+create table ITEM_CATEGORY_MAPPING_MM(
+ mapping_id bigint not null,
+ category_id bigint not null,
+ item_id bigint not null,
+ created_date date,
+ version bigint
+);
+
+--PK
+ALTER TABLE ITEM_CATEGORY_MAPPING_MM ADD PRIMARY KEY(mapping_id);
+
+--item FK
+ALTER TABLE ITEM_CATEGORY_MAPPING_MM ADD CONSTRAINT MAP_ITEM_FK
+ FOREIGN KEY (item_id)
+ REFERENCES ITEM_MANYTOMANY(item_id);
+ 
+ --category FK
+ALTER TABLE ITEM_CATEGORY_MAPPING_MM ADD CONSTRAINT MAP_CATEGORY_FK
+ FOREIGN KEY (category_id)
+ REFERENCES CATEGORY_MANYTOMANY(category_id);
