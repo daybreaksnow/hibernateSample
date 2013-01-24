3668381b71907c1000dd055aae21d06acae1e679
TOSHIYUKI IMAIZUMI <daybreaksnow@gmail.com>
2013-01-24 21:05:40 +0900

    分離オブジェクトのサンプル追加

---
+++ src/detach/DetachSample.java
@@ -0,0 +1,90 @@
+package detach;
+
+import inaction.association.Bid;
+import inaction.association.Item;
+
+import org.hibernate.LockOptions;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import techscore.sample.DaoSupport;
+
+public class DetachSample {
+
+	public static void main(String[] args) {
+		update();
+		lock();
+		refs();
+	}
+
+	private static void update() {
+		DaoSupport dao = new DaoSupport();
+		Session session = dao.getSession();
+		Transaction tx = session.beginTransaction();
+
+		// loadだとプロキシが返されるが、プロキシでも分離した後普通に使える
+		Item item = (Item) session.load(Item.class, 1L);
+		item.setName("1");
+		tx.commit();
+		session.close();
+
+		System.out.println(item.getName());
+
+		session = dao.getSession();
+		tx = session.beginTransaction();
+		item.setName("2");
+		// もし値が変わってなくとも強制的にupdate文は発行される
+		session.update(item);
+
+		tx.commit();
+		session.close();
+	}
+
+	private static void lock() {
+		DaoSupport dao = new DaoSupport();
+		Session session = dao.getSession();
+		Transaction tx = session.beginTransaction();
+
+		Item item = (Item) session.get(Item.class, 1L);
+		item.setName("1");
+		tx.commit();
+		session.close();
+
+		session = dao.getSession();
+		tx = session.beginTransaction();
+		// ロック前の変更は反映されない。ロック後にほかの変更があれば反映される→もし更新しないならばセッションに結び付ける意味があるのか？
+		item.setName("before lock");
+		session.buildLockRequest(LockOptions.READ).lock(item);
+
+		tx.commit();
+		session.close();
+	}
+
+	private static void refs() {
+		DaoSupport dao = new DaoSupport();
+		Session session = dao.getSession();
+		Transaction tx = session.beginTransaction();
+
+		Item item = (Item) session.get(Item.class, 1L);
+		item.setName("1");
+
+		Bid bid = new Bid();
+		bid.setAmount(1L);
+		session.save(bid);
+
+		tx.commit();
+		session.close();
+
+		session = dao.getSession();
+		tx = session.beginTransaction();
+		session.update(item);
+		// 永続化しているものに関連付けると自動的に永続化されるようだ
+		item.getBids().add(bid);
+		bid.setItem(item);
+		bid.setItemId(item.getItemId());
+		bid.setAmount(2L);
+
+		tx.commit();
+		session.close();
+	}
+}
